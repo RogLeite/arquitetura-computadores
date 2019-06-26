c632a3fd84a360bb67ab41bdcaaa3f139f3b5529
RogLeite <rodrigo.ldasilva@hotmail.com>
2019-06-26 08:34:47 -0300

    deixa ram assíncrona e reescreve o programa
    ` a=13;
      b=a;
      a=14;
      a=a-b;
      pos30=a;
      if(a==0)
      {
        a=10;
        pos30=a;
      }
    HALT; `

--- PROJETO_1413150/RAM.vhd
+++ PROJETO_1413150/RAM.vhd
@@ -31,7 +31,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity RAM is
     Port ( reset : in  STD_LOGIC;
-			  clk : in STD_LOGIC;
 			  read_enabled : in  STD_LOGIC;
            write_enabled : in  STD_LOGIC;
            toramdata : in STD_LOGIC_VECTOR (9 downto 0);
@@ -42,32 +41,44 @@ end RAM;
 
 architecture BehavioralRAM of RAM is
 
-type ram_type is array (0 to (2**5-1)) of std_logic_vector(9 downto 0);
+type ram_type is array (0 to (2**5-1)) of std_logic_vector(4 downto 0);
 signal ram: ram_type := (others=>(others=>'0'));
-signal currentAddress: std_logic_vector(4 downto 0):= "00000";
+--signal currentAddress: std_logic_vector(4 downto 0):= "00000";
 signal x: std_logic_vector(4 downto 0):= "11111";
 begin
 
-process(CLK, RESET)
+process(RESET)
 begin
-	if reset = '0' then
-		ram(0)  <= "0000110000";			--Mov A,			(16)
-		ram(1)  <= "00100" & x;				--Mov B,			A
-		ram(2)  <= "0001011110";			--Mov (31),		A
-		ram(3)  <= "00101" & x;				--Add A,			B
-		ram(4)  <= "0110000100";			--JZ	4
-		ram(16) <= "0000001101";
-		ram(31) <= "0111011110"; 			--JMP 31
-	elsif rising_edge(CLK) then
+	if reset = '0' then				-- Os endereços no comentário estão indexados em 0
+		ram(0)  <= "00001";			--Mov A,[add]
+		ram(1)  <= "10000";			--(16)
+		ram(2)  <= "00100";			--Mov B, A
+		ram(3)  <= "00001";			--Mov A,[add]
+		ram(4)  <= "11111";			--(31)
+		ram(5)  <= "00110";			--Sub A, B
+		ram(6)  <= "00010";			--Mov [add], a
+		ram(7)  <= "11110";			--(30)
+		ram(8)  <= "01100";			--JZ [add]
+		ram(9)  <= "01011";			--(11)
+		ram(10) <= "01110";			--HALT
+		ram(11) <= "00001";			--Mov A,[add]
+		ram(12) <= "10001";			--(17)
+		ram(13) <= "00010";			--Mov [add], a
+		ram(14) <= "11110";			--(30)
+		ram(15) <= "01110";			--HALT
+		ram(16) <= "01101";			--#13
+		ram(17) <= "01010";			--#10
+		ram(30) <= "10101";			--#21
+		ram(31) <= "01110"; 			--#14
+	else
 		if (write_enabled = '1') then
 			ram(to_integer(unsigned(address))) <= toramdata;
 		end if;
-		currentAddress <= ADDRESS;
 	end if;
 end process;
 
 
-fromramdata <= ram(to_integer(unsigned(currentAddress)));
+fromramdata <= ram(to_integer(unsigned(address)));
 pos30 <= ram(30)(4 downto 0);
 
 end BehavioralRAM;
@@ -78,4 +89,3 @@ begin
 
 
 end Behavioral;
-
